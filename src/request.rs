82f72acaed2f9d63e60accf36ed0a7bb0d1e43ca
Bill Fraser <wfraser@codewise.org>
2022-12-05 21:45:42 -0800

    add a Debug impl for LogRequest

--- src/request.rs
+++ src/request.rs
@@ -14,6 +14,7 @@ use crate::escaped::Escaped;
 ///
 /// The `A` type parameter is the type of the `action` field, whose `Display`
 /// representation is used when logging.
+#[derive(Debug)]
 pub struct LogRequest<A: Display> {
     start_time: Instant,
     logged: bool,
